d07f12c3a27fca0d6967f08155e4b5f78380851a
Gael Costilla Garcia <gaelcostila@gmail.com>
2025-02-04 13:52:08 -0600

    FIRST COMMIT

---
+++ src/main/java/com/example/miprimerapp2025/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.miprimerapp2025
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.miprimerapp2025.ui.theme.MiPrimerApp2025Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiPrimerApp2025Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android ",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Button(
+        onClick = {
+            println("Hola mundo")
+        }
+    )
+}
+
+fun Button(onClick: () -> Unit) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MiPrimerApp2025Theme {
+        Greeting("Android")
+    }
+}
