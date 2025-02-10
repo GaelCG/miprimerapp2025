ca626af49089e1552cd0cca31f9dc8f1d3b2502a
Gael Costilla Garcia <gaelcostila@gmail.com>
2025-02-10 13:37:14 -0600

    Actualizacion dia 10 Feb

--- src/main/java/com/example/miprimerapp2025/MainActivity.kt
+++ src/main/java/com/example/miprimerapp2025/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -11,7 +13,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.miprimerapp2025.ui.theme.MiPrimerApp2025Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,38 +24,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MiPrimerApp2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android ",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GreetingPreview()
                 }
             }
         }
-    }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    Button(
-        onClick = {
-            println("Hola mundo")
-        }
-    )
-}
 
-fun Button(onClick: () -> Unit) {
-
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MiPrimerApp2025Theme {
-        Greeting("Android")
+    Content("Camaron","Caramelo")
+}
+@Composable
+fun Content(mensaje1:String,mensaje2:String){
+    Column{
+        Text(mensaje1, fontSize = 30.sp)
+        Text(mensaje2, lineHeight = 30.sp)
+        Text("Luis",  fontSize = 30.sp,
+            lineHeight = 32.sp);
+
     }
+
 }
