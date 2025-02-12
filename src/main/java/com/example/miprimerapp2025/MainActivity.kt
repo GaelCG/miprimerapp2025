63476d8b5fdcbb47323c98dc54a6f95f19c118f5
Gael Costilla Garcia <gaelcostila@gmail.com>
2025-02-12 13:41:39 -0600

    CLASE FORMULARIO DE AGREGADO DE ICONOS y botones

--- src/main/java/com/example/miprimerapp2025/MainActivity.kt
+++ src/main/java/com/example/miprimerapp2025/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -24,6 +29,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,14 +38,24 @@ import androidx.compose.ui.unit.dp
 import com.example.miprimerapp2025.ui.theme.MiPrimerApp2025Theme
 
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MiPrimerApp2025Theme {
-                PreviewTextFieldExamples()
+                PreviewExamples()
             }
         }
+    }
+}
+
+@Composable
+fun Examples() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        TextFieldExamples()
+        Spacer(modifier = Modifier.height(20.dp))
+        ButtonExamples()
     }
 }
 
@@ -110,9 +127,111 @@ fun TextFieldWithIcons() {
     )
 }
 
+@Composable
+fun ButtonExamples() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        SimpleButton()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithColor()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithTwoTextView()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithIcon()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithRectangleShape()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithRoundCornerShape()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithCutCornerShape()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithBorder()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonWithElevation()
+    }
+}
+
+@Composable
+fun SimpleButton() {
+    Button(onClick = {}) {
+        Text(text = "Simple Button")
+    }
+}
+
+@Composable
+fun ButtonWithColor() {
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
+    ) {
+        Text(text = "Button with gray background", color = Color.White)
+    }
+}
+
+@Composable
+fun ButtonWithTwoTextView() {
+    Button(onClick = {}) {
+        Text(text = "Click ", color = Color.Magenta)
+        Text(text = "Here", color = Color.Green)
+    }
+}
+
+@Composable
+fun ButtonWithIcon() {
+    Button(onClick = {}) {
+        Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon")
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Add to cart")
+    }
+}
+
+@Composable
+fun ButtonWithRectangleShape() {
+    Button(onClick = {}, shape = RectangleShape) {
+        Text(text = "Forma de Rectangulo")
+    }
+}
+
+@Composable
+fun ButtonWithRoundCornerShape() {
+    Button(onClick = {}, shape = RoundedCornerShape(20.dp)) {
+        Text(text = "Esquina redondeada")
+    }
+}
+
+@Composable
+fun ButtonWithCutCornerShape() {
+    Button(onClick = {}, shape = CutCornerShape(10.dp)) {
+        Text(text = "Esquina en corte")
+    }
+}
+
+@Composable
+fun ButtonWithBorder() {
+    Button(
+        onClick = {},
+        border = BorderStroke(1.dp, Color.Red),
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
+    ) {
+        Text(text = "Boton con borde", color = Color.DarkGray)
+    }
+}
+
+@Composable
+fun ButtonWithElevation() {
+    Button(
+        onClick = {},
+        elevation = ButtonDefaults.elevatedButtonElevation(
+            defaultElevation = 10.dp,
+            pressedElevation = 15.dp,
+            disabledElevation = 0.dp
+        )
+    ) {
+        Text(text = "Button con elevacion")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun PreviewTextFieldExamples() {
-    TextFieldExamples()
-
+fun PreviewExamples() {
+    Examples()
 }
