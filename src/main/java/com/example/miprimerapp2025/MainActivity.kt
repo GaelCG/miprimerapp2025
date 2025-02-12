f72675e511e5736ec1167ec19e44af81f132a354
Gael Costilla Garcia <gaelcostila@gmail.com>
2025-02-12 13:29:08 -0600

    CLASE FORMULARIO DE AGREGADO DE ICONOS

--- src/main/java/com/example/miprimerapp2025/MainActivity.kt
+++ src/main/java/com/example/miprimerapp2025/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,72 +25,94 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.miprimerapp2025.ui.theme.MiPrimerApp2025Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             MiPrimerApp2025Theme {
-
-                    Greeting()
-                }
+                PreviewTextFieldExamples()
             }
         }
     }
+}
 
-
-@Preview(showBackground = true)
 @Composable
-fun Greeting() {
-    Column {
-        textFieldSencillo()
-        textFieldHolder()
-        textFieldKeyboard()
+fun TextFieldExamples() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        SimpleTextField()
+        Spacer(modifier = Modifier.height(10.dp))
+        LabelAndPlaceHolder()
+        Spacer(modifier = Modifier.height(10.dp))
+        TextFieldWithInputType()
+        Spacer(modifier = Modifier.height(10.dp))
+        OutLineTextFieldSample()
+        Spacer(modifier = Modifier.height(10.dp))
+        TextFieldWithIcons()
     }
 }
 
+@Composable
+fun SimpleTextField() {
+    var texto by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = texto,
+        onValueChange = { nuevoTexto -> texto = nuevoTexto }
+    )
+}
 
 @Composable
-fun textFieldSencillo() {
-    var text by remember { mutableStateOf("Hola") }
+fun LabelAndPlaceHolder() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
     TextField(
         value = text,
-        onValueChange = { newText ->
-            text = newText
-        },
-        label = { Text("Introduce tu nombre") }
+        onValueChange = { text = it },
+        label = { Text(text = "Your Label") },
+        placeholder = { Text(text = "Your Placeholder/Hint") },
     )
-    Text(text = "tu nombre es: $text!")
 }
+
 @Composable
-fun textFieldHolder() {
-    var text by remember { mutableStateOf("") }
+fun TextFieldWithInputType() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
     TextField(
         value = text,
-        onValueChange = { newText ->
-            text = newText
-        },
-        label = { Text("Nombre") },
-        placeholder = { Text("Introduce tu Nombre") }
-
+        onValueChange = { text = it },
+        label = { Text(text = "Number Input Type") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
-    Text(text = "tu nombre es: $text!")
 }
+
 @Composable
-fun textFieldKeyboard() {
-    var text by remember { mutableStateOf("") }
-    TextField(
-        text, { newText ->
-            text = newText
-        },
-        label = { Text("Numero") },
-        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
-
-
-
+fun OutLineTextFieldSample() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(text = "Enter Your Name") }
     )
-    Text(text = "tu numero es es: $text!")
+}
+
+@Composable
+fun TextFieldWithIcons() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(text = "Email address") },
+        placeholder = { Text(text = "Enter your e-mail") },
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTextFieldExamples() {
+    TextFieldExamples()
+
 }
