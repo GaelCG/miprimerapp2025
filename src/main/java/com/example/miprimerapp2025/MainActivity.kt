3f002bf7110992770080c3fd539444f23aa08610
Gael Costilla Garcia <gaelcostila@gmail.com>
2025-02-14 13:48:47 -0600

    modificadores

--- src/main/java/com/example/miprimerapp2025/MainActivity.kt
+++ src/main/java/com/example/miprimerapp2025/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -34,6 +37,7 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.miprimerapp2025.ui.theme.MiPrimerApp2025Theme
 
@@ -44,194 +48,70 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MiPrimerApp2025Theme {
-                PreviewExamples()
+                Content()
+                Content2()
+                Content3()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Examples() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        TextFieldExamples()
-        Spacer(modifier = Modifier.height(20.dp))
-        ButtonExamples()
-    }
-}
-
-@Composable
-fun TextFieldExamples() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        SimpleTextField()
-        Spacer(modifier = Modifier.height(10.dp))
-        LabelAndPlaceHolder()
-        Spacer(modifier = Modifier.height(10.dp))
-        TextFieldWithInputType()
-        Spacer(modifier = Modifier.height(10.dp))
-        OutLineTextFieldSample()
-        Spacer(modifier = Modifier.height(10.dp))
-        TextFieldWithIcons()
-    }
-}
-
-@Composable
-fun SimpleTextField() {
-    var texto by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        value = texto,
-        onValueChange = { nuevoTexto -> texto = nuevoTexto }
+fun Content(){
+    Text("GAEL COSTILLA GARCIA",
+            Modifier.padding(32.dp) // Outer padding (margin)
+        .background(color = Color.Green) //background color
+        .padding(16.dp) // Inner padding
     )
 }
-
+@Preview(showBackground = true)
 @Composable
-fun LabelAndPlaceHolder() {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text(text = "Your Label") },
-        placeholder = { Text(text = "Your Placeholder/Hint") },
-    )
-}
+fun Content2(){
+    Text("GAEL COSTILLA GARCIA",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Blue)
+            .width(200.dp)
+            .height(300.dp))
 
-@Composable
-fun TextFieldWithInputType() {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text(text = "Number Input Type") },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-    )
-}
-
-@Composable
-fun OutLineTextFieldSample() {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text(text = "Enter Your Name") }
-    )
-}
-
-@Composable
-fun TextFieldWithIcons() {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text(text = "Email address") },
-        placeholder = { Text(text = "Enter your e-mail") },
-        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") }
-    )
-}
-
-@Composable
-fun ButtonExamples() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        SimpleButton()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithColor()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithTwoTextView()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithIcon()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithRectangleShape()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithRoundCornerShape()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithCutCornerShape()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithBorder()
-        Spacer(modifier = Modifier.height(10.dp))
-        ButtonWithElevation()
-    }
-}
-
-@Composable
-fun SimpleButton() {
-    Button(onClick = {}) {
-        Text(text = "Simple Button")
-    }
-}
-
-@Composable
-fun ButtonWithColor() {
-    Button(
-        onClick = {},
-        colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
-    ) {
-        Text(text = "Button with gray background", color = Color.White)
-    }
-}
-
-@Composable
-fun ButtonWithTwoTextView() {
-    Button(onClick = {}) {
-        Text(text = "Click ", color = Color.Magenta)
-        Text(text = "Here", color = Color.Green)
-    }
-}
-
-@Composable
-fun ButtonWithIcon() {
-    Button(onClick = {}) {
-        Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon")
-        Spacer(modifier = Modifier.width(10.dp))
-        Text(text = "Add to cart")
-    }
-}
-
-@Composable
-fun ButtonWithRectangleShape() {
-    Button(onClick = {}, shape = RectangleShape) {
-        Text(text = "Forma de Rectangulo")
-    }
-}
-
-@Composable
-fun ButtonWithRoundCornerShape() {
-    Button(onClick = {}, shape = RoundedCornerShape(20.dp)) {
-        Text(text = "Esquina redondeada")
-    }
-}
-
-@Composable
-fun ButtonWithCutCornerShape() {
-    Button(onClick = {}, shape = CutCornerShape(10.dp)) {
-        Text(text = "Esquina en corte")
-    }
-}
-
-@Composable
-fun ButtonWithBorder() {
-    Button(
-        onClick = {},
-        border = BorderStroke(1.dp, Color.Red),
-        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
-    ) {
-        Text(text = "Boton con borde", color = Color.DarkGray)
-    }
-}
-
-@Composable
-fun ButtonWithElevation() {
-    Button(
-        onClick = {},
-        elevation = ButtonDefaults.elevatedButtonElevation(
-            defaultElevation = 10.dp,
-            pressedElevation = 15.dp,
-            disabledElevation = 0.dp
-        )
-    ) {
-        Text(text = "Button con elevacion")
-    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewExamples() {
-    Examples()
+fun Content3(){
+    Text("GAEL COSTILLA GARCIA",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Cyan)
+            .size(width = 250.dp, height = 100.dp)
+    )
+
 }
+
+@Preview(showBackground = true)
+@Composable
+fun Content4(){
+    Text("GAEL COSTILLA GARCIA",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Gray)
+            .padding(Dp(10f))
+            .fillMaxWidth(1f)
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Content5(){
+    Text("GAEL COSTILLA GARCIA",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Gray)
+            .padding(Dp(10f))
+            .fillMaxWidth(1f)
+    )
+
+}
+
